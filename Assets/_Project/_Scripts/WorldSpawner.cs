c2f1f097b33e83ba02cd974632a8716fc99892d5
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-11-19 13:39:04 +0200

    Fixed object spawning.

--- Assets/_Project/_Scripts/WorldSpawner.cs
+++ Assets/_Project/_Scripts/WorldSpawner.cs
@@ -17,36 +17,40 @@ public class WorldSpawner : MonoBehaviour
         Instance = this;
     }
 
+    private bool CheckIfWorldPointIsInsidePlane(ARPlane plane, Vector3 worldPosition)
+    {
+        var local = plane.transform.InverseTransformPoint(worldPosition);
+
+        if (local.x >= -plane.extents.x && local.x <= plane.extents.x && local.z >= -plane.extents.y && local.z <= plane.extents.y)
+            return true;
+        return false;
+    }
+
     public void SpawnObjects()
     {
         var userPosition = Camera.main.transform.position;
         var planes = new List<ARPlane>();
         foreach (var plane in planeManager.trackables)
             if (plane.alignment == UnityEngine.XR.ARSubsystems.PlaneAlignment.HorizontalUp)
-            {
-                var planeTopY = plane.transform.position.y + plane.extents.y;
-                var planeTopX = plane.transform.position.x + plane.extents.x;
-                var planeBottomY = plane.transform.position.y - plane.extents.y;
-                var planeBottomX = plane.transform.position.y - plane.extents.x;
-                var userLocal = plane.transform.worldToLocalMatrix.MultiplyPoint(userPosition);
-                var userX = userLocal.x;
-                var userY = userLocal.y;
-
-                if (userY <= planeTopY && userY >= planeBottomY && userX <= planeTopX && userX >= planeBottomX)
+                if (CheckIfWorldPointIsInsidePlane(plane, userPosition))
                     planes.Add(plane);
-            }
 
-        var points = new List<Vector3>();
         for (int i = 0; i < objectsToSpawn; i++)
         {
             var inCylinder = Random.insideUnitCircle * spawnRadius;
-            var plane = planes[Random.Range(0, planes.Count)].transform;
+            var x = inCylinder.x + userPosition.x;
+            var z = inCylinder.y + userPosition.z;
+            var appropriatePlanes = planes.Where(plane => CheckIfWorldPointIsInsidePlane(plane, new(x, 0f, z))).ToArray();
+            if (appropriatePlanes.Length == 0)
+            {
+                i--;
+                continue;
+            }
+            var plane = appropriatePlanes[Random.Range(0, appropriatePlanes.Length)].transform;
             var a = plane.up.x;
             var b = plane.up.y;
             var c = plane.up.z;
-            var d = -(a * plane.position.x + b * plane.position.y + c * plane.position.z);
-            var x = inCylinder.x;
-            var z = inCylinder.y;
+            var d = -(a * plane.position.x + b * plane.position.y + c * plane.position.z); 
             var y = -(a * x + c * z + d) / b;
             Instantiate(objectToSpawn, new Vector3(x, y, z), Quaternion.identity);
         }
